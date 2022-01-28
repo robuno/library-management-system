d1dff5ceaab7aa6f2786ee8731a25801e7d8a9d4
Unat Teksen <unatteksen@hotmail.com>
2022-01-29 00:06:37 +0300

    final remove

--- src/com/teksen/IssueReturn.java
+++
@@ -1,9 +0,0 @@
-package com.teksen;
-
-import javax.swing.table.DefaultTableModel;
-import java.util.ArrayList;
-
-public interface IssueReturn {
-    void pressDeleteDigitalButton(DefaultTableModel modelTable, ArrayList<ArrayList<String>> itemsArrayList);
-    void pressAddDigitalButton(DefaultTableModel modelTable,  int lastID);
-}
